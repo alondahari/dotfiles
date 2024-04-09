4f7ba1e4ed1b71c4f4323214da307ce841e79a37
Alon Dahari <climbinghobo@gmail.com>
2024-04-09 21:58:28 +0300

    Use mason for lsp management

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -2,50 +2,66 @@ return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
     {
-      'hrsh7th/cmp-nvim-lsp',
-      branch = 'main',
+      "hrsh7th/cmp-nvim-lsp",
+      branch = "main",
       dependencies = {
-        { 'hrsh7th/nvim-cmp', branch = 'main' }
-      }
+        { "hrsh7th/nvim-cmp", branch = "main" },
+      },
     },
   },
   config = function()
-    local nvim_lsp = require('lspconfig')
+    require("mason").setup()
+    local registry = require("mason-registry")
+    local lsp_config = require("mason-lspconfig")
+    lsp_config.setup()
+
+    local nvim_lsp = require("lspconfig")
 
     -- Use an on_attach function to only map the following keys
     -- after the language server attaches to the current buffer
     local on_attach = function(client, bufnr)
-      local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+      local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+      end
 
-      local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+      local function buf_set_option(...)
+        vim.api.nvim_buf_set_option(bufnr, ...)
+      end
 
       -- Enable completion triggered by <c-x><c-o>
-      buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+      buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
       -- Mappings.
       local opts = { noremap = true, silent = true }
 
       -- See `:help vim.lsp.*` for documentation on any of the below functions
-      buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float({scope="line"})<CR>', opts)
-      buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setqflist()<CR>', opts)
-      buf_set_keymap('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-      buf_set_keymap('x', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-      buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-      buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-      buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-      buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-      buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-      buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-      buf_set_keymap('n', '<C-;>', '<cmd>lua vim.diagnostic.goto_prev({ float = false })<CR>', opts)
-      buf_set_keymap('n', '<C-o>', '<cmd>lua vim.diagnostic.goto_next({ float = false })<CR>', opts)
-      buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts)
+      buf_set_keymap("n", "<space>e", '<cmd>lua vim.diagnostic.open_float({scope="line"})<CR>', opts)
+      buf_set_keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setqflist()<CR>", opts)
+      buf_set_keymap("n", "<space>a", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+      buf_set_keymap("x", "<space>a", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+      buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+      buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+      buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+      buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+      buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+      buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+      buf_set_keymap("n", "<C-;>", "<cmd>lua vim.diagnostic.goto_prev({ float = false })<CR>", opts)
+      buf_set_keymap("n", "<C-o>", "<cmd>lua vim.diagnostic.goto_next({ float = false })<CR>", opts)
+      buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
 
-      buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-      buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-      buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-      buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-      buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+      buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+      buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+      buf_set_keymap(
+        "n",
+        "<space>wl",
+        "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+        opts
+      )
+      buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+      buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 
       -- nnoremap <silent> gd    <cmd>lua vim.lsp.buf.definition()<CR>
       -- nnoremap <silent> gi    <cmd>lua vim.lsp.buf.implementation()<CR>
@@ -58,18 +74,43 @@ return {
       -- nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
     end
 
+    local ensured_installed_packages = {
+      "lua-language-server",
+
+      "css-lsp",
+      "html-lsp",
+      "json-lsp",
+      "emmet-ls",
+
+      "typescript-language-server",
+      "prettier",
+
+      "yaml-language-server",
+      "yamlfmt",
+      "yamllint",
+    }
+
+    for _, package in ipairs(ensured_installed_packages) do
+      local p = registry.get_package(package)
+
+      if not p:is_installed() then
+        p:install()
+      end
+    end
+
     -- Use a loop to conveniently call 'setup' on multiple servers and
     -- map buffer local keybindings when the language server attaches
-    local servers = { 'tsserver', 'clojure_lsp', 'sourcekit', 'pyright', 'vimls', 'dockerls', 'terraformls', 'graphql',
-      'dartls', 'lua_ls', 'prismals', 'ltex', 'ruby_ls', 'eslint' }
+    local servers = { "graphql", "ruby_ls", "gopls", "tsserver" }
     for _, lsp in ipairs(servers) do
-      nvim_lsp[lsp].setup {
+      nvim_lsp[lsp].setup({
         on_attach = on_attach,
         flags = {
           debounce_text_changes = 150,
         },
-        capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-      }
+        capabilities = require("cmp_nvim_lsp").default_capabilities(
+          vim.lsp.protocol.make_client_capabilities()
+        ),
+      })
     end
 
     local rust_opts = {
@@ -94,25 +135,26 @@ return {
           ["rust-analyzer"] = {
             -- enable clippy on save
             checkOnSave = {
-              command = "clippy"
+              command = "clippy",
             },
-          }
-        }
+          },
+        },
       },
     }
 
-    require('rust-tools').setup(rust_opts)
-    require 'lspconfig'.lua_ls.setup {
+    require("rust-tools").setup(rust_opts)
+    -- go lsp
+    require("lspconfig").lua_ls.setup({
       settings = {
         Lua = {
           diagnostics = {
-            globals = { 'vim' }
-          }
-        }
-      }
-    }
+            globals = { "vim" },
+          },
+        },
+      },
+    })
 
-    nvim_lsp.ccls.setup {
+    nvim_lsp.ccls.setup({
       init_options = {
         compilationDatabaseDirectory = "build",
         index = {
@@ -121,29 +163,29 @@ return {
         clang = {
           excludeArgs = { "-frounding-math" },
         },
-      }
-    }
+      },
+    })
 
     -- ===========================================
     --  Add user dictionary for ltex-ls
     --  * en.utf-8.add must be created using `zg` when set spell is on
     -- ===========================================
-    nvim_lsp.ltex.setup {
+    nvim_lsp.ltex.setup({
       on_attach = on_attach,
       settings = {
         ltex = {
           language = "en-GB",
-          dictionary = { ['en-GB'] = {} },
+          dictionary = { ["en-GB"] = {} },
           additionalRules = {
-            languageModel = '~/ngrams/',
-          }
-        }
-      }
-    }
-
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      pattern = '*',
-      command = 'lua vim.lsp.buf.format()'
+            languageModel = "~/ngrams/",
+          },
+        },
+      },
     })
-  end
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      command = "lua vim.lsp.buf.format()",
+    })
+  end,
 }
