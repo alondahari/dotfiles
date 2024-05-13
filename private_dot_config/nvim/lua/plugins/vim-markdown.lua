982bc0718ae3e045a20973db9751d89df477fe49
Alon Dahari <climbinghobo@gmail.com>
2024-05-13 22:33:44 +0100

    Update md pacakge
    I actually like the # at the beginning of the heading.

--- private_dot_config/nvim/lua/plugins/vim-markdown.lua
+++ private_dot_config/nvim/lua/plugins/vim-markdown.lua
@@ -1,8 +1,11 @@
 return {
 	"MeanderingProgrammer/markdown.nvim",
 	name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+	ft = { "markdown" },
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	config = function()
-		require("render-markdown").setup({})
+		require("render-markdown").setup({
+			headings = { "# ", "## ", "### ", "#### ", "##### ", "###### " },
+		})
 	end,
 }
