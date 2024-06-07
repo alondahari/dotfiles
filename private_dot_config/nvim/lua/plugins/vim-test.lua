dd861b2b5f32e2df5362a6f2d9504cb7fa587ff3
Alon Dahari <climbinghobo@gmail.com>
2024-06-07 14:57:16 +0100

    Update keymap to test all features

--- private_dot_config/nvim/lua/plugins/vim-test.lua
+++ private_dot_config/nvim/lua/plugins/vim-test.lua
@@ -7,7 +7,7 @@ return {
 
 		vim.keymap.set('n', '<leader>t', '<cmd>:TestNearest<CR>', { silent = true })
 		vim.keymap.set('n', '<leader>T', '<cmd>:TestFile<CR>', { silent = true })
-		vim.keymap.set('n', '<leader>a', '<cmd>:TestSuite<CR>', { silent = true })
+		vim.keymap.set('n', '<leader>a', '<cmd>:TestNearest TEST_ALL_FEATURES=1<CR>', { silent = true })
 		vim.keymap.set('n', '<leader>l', '<cmd>:TestLast<CR>', { silent = true })
 		vim.keymap.set('n', '<leader>v', '<cmd>:TestVisit<CR>', { silent = true })
 	end
