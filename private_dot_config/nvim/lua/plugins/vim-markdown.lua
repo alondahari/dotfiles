a4662e29e471324c2cc4feeaa54b68ac2a119cc1
Alon Dahari <climbinghobo@gmail.com>
2025-10-23 14:17:37 +0100

    Update markdown and term

--- private_dot_config/nvim/lua/plugins/vim-markdown.lua
+++ private_dot_config/nvim/lua/plugins/vim-markdown.lua
@@ -1,5 +1,5 @@
 return {
-	"MeanderingProgrammer/markdown.nvim",
+	"MeanderingProgrammer/render-markdown.nvim",
 	name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
 	ft = { "markdown" },
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
